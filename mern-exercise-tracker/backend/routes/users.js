8a4b323982e1c7e19b389cd9022c42907e485bad
Mustafa Alvi <mustafa.alvi.98@gmail.com>
2021-07-24 16:25:53 -0400

    Refactored the form object to make username static in the edit-exercise component

--- mern-exercise-tracker/backend/routes/users.js
+++ mern-exercise-tracker/backend/routes/users.js
@@ -19,5 +19,11 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req,res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err)); 
+});
+
 
 module.exports = router;
