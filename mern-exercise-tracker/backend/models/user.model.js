bc9041093dcd6adeaaa02950404ff6af1b49d4fc
Mustafa Alvi <mustafa.alvi.98@gmail.com>
2021-07-28 17:11:33 -0400

    Mongoose formValidation beautifier package for newUser

--- mern-exercise-tracker/backend/models/user.model.js
+++ mern-exercise-tracker/backend/models/user.model.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose'); 
+const beautifyUnique = require('mongoose-beautiful-unique-validation'); 
 
 const Schema = mongoose.Schema; 
 
 const userSchema = new Schema({
-    username: {type: String, required: true, unique: true, trim: true, minlength: 3},
+    username: {
+        type: String, 
+        required: true, 
+        unique: 'Two users cannot share the same username ({VALUE})', 
+        trim: true, 
+        minlength: 3},
     height: {type: String, required: false, unique: false, trim: true, minlength:4},
     weight: {type: Number, required: false, unique: false, trim: true}
 }, {
     timestamps: true,
 }); 
+
+userSchema.plugin(beautifyUnique);
 
 const User = mongoose.model('User', userSchema); 
 
