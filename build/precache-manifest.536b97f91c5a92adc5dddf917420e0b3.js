43905cab0c68399eb4f7ce06d296408d4291a7c2
Mustafa Alvi <mustafa.alvi.98@gmail.com>
2021-08-19 19:17:31 -0400

    Build commit

---
+++ build/precache-manifest.536b97f91c5a92adc5dddf917420e0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6aec2cf38b859bf0551c132df60d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e68233bab7c617011c45",
+    "url": "/static/css/2.ce4e4d3b.chunk.css"
+  },
+  {
+    "revision": "4383aed3a8d3c8b5bb35",
+    "url": "/static/css/main.984651fe.chunk.css"
+  },
+  {
+    "revision": "e68233bab7c617011c45",
+    "url": "/static/js/2.fee03f67.chunk.js"
+  },
+  {
+    "revision": "3f2e1ff0d15dc835e00724af8791d834",
+    "url": "/static/js/2.fee03f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4383aed3a8d3c8b5bb35",
+    "url": "/static/js/main.742dca57.chunk.js"
+  },
+  {
+    "revision": "a37af620878b53014277",
+    "url": "/static/js/runtime-main.0af7428b.js"
+  }
+]);
